8add36982e6de0d667204bc536c8fed395e2d2af
gotojo <MXI03066@gmail.com>
2023-02-21 20:28:31 +0900

    add audio files to service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -2,6 +2,25 @@
 var CACHE_NAME = 'pwa-caches';
 var urlsToCache = [
     '/gotojo.github.io/Guts-Daze',
+    '/gotojo.github.io/Guts-Daze/Media/BridgeBPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/BridgeBPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/EndingBPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/EndingBPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/Guts1BPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/Guts1BPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/Guts2BPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/Guts2BPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/IntroBPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/IntroBPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloBesoBPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloBesoBPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloEndingBPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloEndingBPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloGuts1BPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloGuts1BPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloGuts2BPM105.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/MeloGuts2BPM123_5.m4a',
+    '/gotojo.github.io/Guts-Daze/Media/bubu.m4a'
 ];
 
 // インストール処理
